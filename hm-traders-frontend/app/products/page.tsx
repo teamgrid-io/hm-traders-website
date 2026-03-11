707f41354ecfa93f09a26f32767d6b03cbc1952f
Sakil7430 <sakil.hossain@teamgrid.io>
2026-03-11 13:22:49 +0530

    updated

---
+++ hm-traders-frontend/app/products/page.tsx
@@ -0,0 +1,11 @@
+import ProductCategories from "@/components/home/ProductCategories";
+import Container from "@/components/layout/Container";
+const page = () => {
+  return (
+    <Container>
+      <ProductCategories />
+    </Container>
+  )
+}
+
+export default page
